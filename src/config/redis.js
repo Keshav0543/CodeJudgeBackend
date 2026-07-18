ce3f1855706493e4b01ad34bd8abc33490069106
Keshav Thakur <keshavt043@gmail.com>
2026-07-18 21:06:35 +0530

    update18-7-26

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,8 +6,8 @@ const client = createClient({
     username: 'default',
     password: process.env.REDIS_P,
     socket: {
-        host: 'redis-16913.c212.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 16913
+        host: 'redis-14749.c212.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 14749
     }
 });
 
